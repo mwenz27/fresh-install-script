ce567e7a06307e77b684b07160c154e037c26015
Michael <you@example.com>
2023-01-19 06:23:44 +0800

    urls and chrome extension added

---
+++ README.md
@@ -0,0 +1,13 @@
+## Fresh Install Python Script
+
+Run using the following commands:
+
+`python main.py windows`
+
+`python main.py mac`
+
+`python main.py linux`
+
+### Chrome and Brave Extensions List
+
+`python main.py chrome`
--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 """
 Create a script to install items without needing to search for the links ever again.
 
-Windows - Opens Browser and opens tabs
-Mac - Opens Browser and opens tabs
-Linux - Runs Linux commands
-
-
-run the code for windows 'python main.py window'
-built 18-01-2023
+run the code for windows 'python main.py windows'
+built on 18-01-2023
 """
 
 import argparse
@@ -15,16 +10,25 @@ import webbrowser
 import subprocess
 
 parser = argparse.ArgumentParser()
-parser.add_argument("arg1", help="specify windows, mac or linux")
+parser.add_argument("arg1", help="specify windows, mac, linux or chrome")
 args = parser.parse_args()
 
 
 def freshinstall():
+    """Windows - Opens Browser and opens tabs
+    Mac - Opens Browser and opens tabs
+    Linux - Runs Linux commands
+    :return:
+    """
     if args.arg1 == "windows":
 
         url_list = [
+            # flux
+            "https://justgetflux.com/",
             # Git, a distributed version control system for software development
             "https://git-scm.com/download/win",
+            # Git GUI
+            "https://www.sourcetreeapp.com/",
             # Software for the Arduino platform
             "https://www.arduino.cc/en/software",
             # Sublime Text, a popular text editor
@@ -62,14 +66,43 @@ def freshinstall():
             "https://slack.com/intl/en-au/download",
             # Exodus, a cryptocurrency wallet
             "https://www.exodus.com/download/",
+            # Malyware bytes, protection for PC
+            "https://www.malwarebytes.com/mwb-download/thankyou",
+            # Trello, board list
+            "https://apps.microsoft.com/store/detail/trello/9NBLGGH4XXVW?hl=en-us&gl=us&rtc=1",
+            # Plex, media server
+            "https://www.plex.tv/media-server-downloads/?lang=en-au#plex-app",
+            # Rescue Time,
+            "https://www.rescuetime.com/download",
+            # Postman, a tool for API development and testing
+            "https://www.postman.com/downloads/",
         ]
 
         for url in url_list:
             webbrowser.open(url)
             print(url)
+            if "https://ninite.com/":
+                print(""" Select Items
+                            Zoom, a popular video conferencing software
+            Discord, a popular communication and chat platform for gamers
+            VLC, a media player
+            Spotify, a music streaming service
+            Krita, a digital painting and illustration software
+            Greenshot, a screenshot software
+            Google Earth, a virtual globe and map software
+            Steam, a digital distribution platform for video games
+            7-zip, a file archiving software
+            Anaconda, a distribution of the Python and R programming languages
+                """
+                      )
+
 
     elif args.arg1 == "mac":
         url_list = [
+            # flux
+            "https://justgetflux.com/",
+            # Sublime Text, a popular text editor
+            "https://www.sublimetext.com/download",
             # Git, a distributed version control system for software development
             "https://git-scm.com/download/win",
             # Visual Studio Code, a text editor and integrated development environment (IDE)
@@ -110,14 +143,20 @@ def freshinstall():
             # 7-zip, a file archiving software
             "https://www.7-zip.org/download.html",
             # Exodus, a cryptocurrency wallet
-            "https://www.exodus.com/download/"
+            "https://www.exodus.com/download/",
+            # Plex, media server
+            "https://www.plex.tv/media-server-downloads/?lang=en-au#plex-app",
+            # Rescue Time, time management
+            "https://www.rescuetime.com/download",
+            # Postman, a tool for API development and testing
+            "https://www.postman.com/downloads/",
         ]
 
         for url in url_list:
             webbrowser.open(url)
             print(url)
 
-    else:
+    elif args.arg1 == "linux":
 
         apps = [
             # Google Chrome, a web browser
@@ -165,6 +204,68 @@ def freshinstall():
         for app in apps:
             subprocess.run(["sudo", "apt-get", "install", "-y", app], shell=False)
             print(app)
+
+    elif args.arg1 == "chrome":
+        extensions = [
+            # AdBlock Plus, an ad blocker
+            ("AdBlock Plus", "https://chrome.google.com/webstore/detail/adblock-plus/cfhdojbkjhnklbpkdaibdccddilifddb"),
+            # Grammarly, a grammar and spelling checker
+            ("Grammarly",
+             "https://chrome.google.com/webstore/detail/grammarly-for-chrome/kbfnbcaeplbcioakkpcpgfkobkghlhen"),
+            # LastPass, a password manager
+            ("LastPass",
+             "https://chrome.google.com/webstore/detail/lastpass-free-password-ma/hdokiejnpimakedhajhdlcegeplioahd"),
+            # uBlock Origin, an ad blocker
+            ("uBlock Origin",
+             "https://chrome.google.com/webstore/detail/ublock-origin/cjpalhdlnbpafiamejdnhcphjbkeiagm"),
+            # Evernote Web Clipper, a note-taking and web-clipping tool
+            ("Evernote Web Clipper", "https://chrome.google.com/webstore/detail"),
+            # Google Translate, a translation tool
+            ("Google Translate",
+             "https://chrome.google.com/webstore/detail/google-translate/aapbdbdomjkkjkaonfhkkikfgjllcleb"),
+            # Save to Pocket, a tool for saving web pages to read later
+            ("Save to Pocket",
+             "https://chrome.google.com/webstore/detail/save-to-pocket/niloccemoadcdkdjlinkgdfekeahmflj"),
+            # Awesome Screenshot, a screen capture tool
+            ("Awesome Screenshot",
+             "https://chrome.google.com/webstore/detail/awesome-screenshot/nlipoenfbbikpbjkfpfillcgkoblgpmj"),
+            # OneTab, a tool for managing tabs
+            ("OneTab", "https://chrome.google.com/webstore/detail/onetab/chphlpgkkbolifaimnlloiipkdnihall"),
+            # The Great Suspender, a tool for suspending tabs to save memory
+            ("The Great Suspender",
+             "https://chrome.google.com/webstore/detail/the-great-suspender/klbibkeccnjlkjkiokjodocebajanakg"),
+            # StayFocusd, a tool for managing time spent on distracting websites
+            ("StayFocusd", "https://chrome.google.com/webstore/detail/stayfocusd/laankejkbhbdhmipfmgcngdelahlfoji"),
+            # RescueTime, a tool for tracking time spent on websites and apps
+            ("RescueTime", "https://chrome.google.com/webstore/detail/rescuetime/bmnlcjabgnpnenekpadlanbbkooimhnj"),
+            # Boomerang for Gmail, a tool for scheduling and tracking emails
+            ("Boomerang for Gmail",
+             "https://chrome.google.com/webstore/detail/boomerang-for-gmail/mdanidgdpmkimeiiojknlnekblgmpdll"),
+            # Todoist, a to-do list and task management tool
+            ("Todoist",
+             "https://chrome.google.com/webstore/detail/todoist-to-do-list-task-l/jldhpllghnbhlleeobkclaookdhncomo"),
+            # Buffer, a tool for scheduling social media posts
+            ("Buffer", "https://chrome.google.com/webstore/detail/buffer/noojglkidnpfjbincgijbaiedldjfbhh"),
+            # Hootsuite Hootlet, a tool for scheduling and tracking social media posts
+            ("Hootsuite Hootlet", "https://chrome.google.com/webstore/detail"),
+            # Full Page Screen Capture, a tool for capturing full webpage screenshots
+            ("Full Page Screen Capture",
+             "https://chrome.google.com/webstore/detail/full-page-screen-capture/fdpohaocaechififmbbbbbknoalclacl"),
+            # Grammarly for Microsoft Office, an extension for grammar and spelling check in Microsoft Office
+            ("Grammarly for Microsoft Office",
+             "https://chrome.google.com/webstore/detail/grammarly-for-microsoft-o/eljapbgkmlngdpckoiiibecpddcmgdlh"),
+            # Tampermonkey, a userscript manager
+            ("Tampermonkey", "https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo"),
+            # Wappalyzer, a tool for identifying web technologies used on websites
+            ("Wappalyzer", "https://chrome.google.com/webstore/detail/wappalyzer/gppongmhjkpfnbhagpmjfkannfbllamg"),
+            # Postman, a tool for API development and testing
+            ("Postman", "https://chrome.google.com/webstore/detail/postman/fhbjgbiflinjbdggehcddcbncdddomop")
+
+        ]
+
+        for extension in extensions:
+            print(extension[0])
+            webbrowser.open(extension[1])
 
 
 if __name__ == '__main__':
